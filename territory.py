dc20e8c68268dbcf4ece8f71dd08f1f2df56668b
PISMATH <sai.bhushan2019@gmail.com>
2024-05-31 11:30:40 +0800

    Add territory render

--- territory.py
+++ territory.py
@@ -1,9 +1,30 @@
+import pygame
+
+from config import *
+from math import sin, cos, pi
+
 class territory:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, pos, color, alpha) -> None:
+        self.points = []
+        self.set_inital_points(pos)
+        self.color = color
+        self.alpha = alpha
+        self.growth_rate = territory_base_growth_rate
+
+    def set_inital_points(self, center):
+        for i in range(territory_inital_vertices_count):
+            angle = 2 * pi * i / territory_inital_vertices_count
+            self.points.append(
+                [center[0] + sin(angle) * territory_inital_size,
+                 center[1] + cos(angle) * territory_inital_size]
+            )
 
     def update(self, dt):
         pass
 
     def render(self, screen):
-        pass
+        render_surface = pygame.Surface(screen.get_size(), pygame.SRCALPHA)
+        color = self.color + [self.alpha]
+        pygame.draw.polygon(render_surface, color, self.points)
+        
+        screen.blit(render_surface, (0, 0))
